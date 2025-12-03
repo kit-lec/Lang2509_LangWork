df04760b4e3bf7bf16b4f72795655e7504443bc8
kit-lec <kit_lec@hotmail.com>
2025-12-03 19:49:21 +0900

    251203-1

--- 12_InvestorGPT/12_04_OpenAIFunctionsAgent.py
+++ 12_InvestorGPT/12_04_OpenAIFunctionsAgent.py
@@ -14,6 +14,14 @@ from pydantic import BaseModel, Field
 from typing import Any, Type  # typing 은 Python 의 기본 내장모듈
 
 
+
+# pydantic 의 BaseModel 구현. => 사용자 정의 데이터 모델 정의
+#   - 입력데이터의 타입검증, 자동변환, 직렬화/역직렬화 등의 처리기능 제공됨.
+class CalculatorToolArgsSchema(BaseModel):
+    a: float = Field(description="The first number")
+    b: float = Field(description="The second number")
+
+
 class CalculatorTool(BaseTool):
     name: Type[str] = "CalculatorTool"   # tool 이름에 공백 있으면 안됨!
     description: Type[str] = """
@@ -22,26 +30,25 @@ class CalculatorTool(BaseTool):
     Only receives two arguments.
     """
 
-    # TODO:  
+    # 매개변수 argument 의 구조(스키마) 정의 
+    # argument 의 스키마를 정의하기 위해 별도의 pydantic Model 을 준비해봅니다 
+    # argument 들은 이 스키마를 따르도록 지정
+    #   => a 는 실수형, b 도 실수형
+    args_schema: Type[CalculatorToolArgsSchema] = CalculatorToolArgsSchema
 
+    # 이 툴이 사용되었을때 실행할 코드
+    def _run(self, a, b):
+        return a + b
 
 
 llm = ChatOpenAI(temperature=0.1)
-
-def plus(inputs):
-    a, b = inputs.split(",")
-    return float(a) + float(b)
 
 agent = initialize_agent(
     llm=llm,
     verbose=True,
     agent=AgentType.OPENAI_FUNCTIONS,
     tools=[
-        Tool.from_function(
-            func=plus,
-            name="Sum Calculator",
-            description="Use this to perform sums of two numbers. Use this tool by sending a pair of numbers separated by a comma.\nExample:1,2",
-        ),
+        CalculatorTool()  # 커스텀 tool 지정
     ],
 )
 
